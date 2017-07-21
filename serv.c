c880d0a979523facc512d3cf590be8759d91efff
19Himanshu <19himanshu96@gmail.com>
2017-07-21 14:29:17 -0700

    Update serv.c

--- serv.c
+++ serv.c
@@ -1,154 +1,59 @@
-#include <stdlib.h>
+/* A simple echo server using TCP */
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/wait.h>
 #include <sys/socket.h>
-#include <signal.h>
-#include <ctype.h>          
-#include <arpa/inet.h>
-#include <netdb.h>
-
-#define PORT 20000 
-#define BACKLOG 5
-#define LENGTH 512 
-
-void error(const char *msg)
+#include <netinet/in.h>
+itdefine SERVER_TCP_POR‘I‘ 3000 /* well—known port */
+#define BUFLEN 256 /* buffer length */
+int main(int argc, char **argv)
 {
-	perror(msg);
-	exit(1);
+int n, bytes_to_read;
+int sd, new_sd, client_len, port;
+struct sockaddr_in server, client;
+char *bp, buf[BUFLEN];
+switch(argc) {
+case 1:
+pOIt = SERVER_TCP_PORT;
+break;
+case 2:
+port = atoi(argv[l]);
+break;
+default:
+fprintf(stderr, "Usage: %s [port]\n", argv[O]);
+exit(l);
 }
-
-int main ()
-{
-	/* Defining Variables */
-	int sockfd; 
-	int nsockfd; 
-	int num;
-	int sin_size; 
-	struct sockaddr_in addr_local; /* client addr */
-	struct sockaddr_in addr_remote; /* server addr */
-	char revbuf[LENGTH]; // Receiver buffer
-
-	/* Get the Socket file descriptor */
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 )
-	{
-		fprintf(stderr, "ERROR: Failed to obtain Socket Descriptor. (errno = %d)\n", errno);
-		exit(1);
-	}
-	else 
-		printf("[Server] Obtaining socket descriptor successfully.\n");
-
-	/* Fill the client socket address struct */
-	addr_local.sin_family = AF_INET; // Protocol Family
-	addr_local.sin_port = htons(PORT); // Port number
-	addr_local.sin_addr.s_addr = INADDR_ANY; // AutoFill local address
-	bzero(&(addr_local.sin_zero), 8); // Flush the rest of struct
-
-	/* Bind a special Port */
-	if( bind(sockfd, (struct sockaddr*)&addr_local, sizeof(struct sockaddr)) == -1 )
-	{
-		fprintf(stderr, "ERROR: Failed to bind Port. (errno = %d)\n", errno);
-		exit(1);
-	}
-	else 
-		printf("[Server] Binded tcp port %d in addr 130.63.96.145 sucessfully.\n",PORT);
-
-	/* Listen remote connect/calling */
-	if(listen(sockfd,BACKLOG) == -1)
-	{
-		fprintf(stderr, "ERROR: Failed to listen Port. (errno = %d)\n", errno);
-		exit(1);
-	}
-	else
-		printf ("[Server] Listening the port %d successfully.\n", PORT);
-
-	int success = 0;
-	while(success == 0)
-	{
-		sin_size = sizeof(struct sockaddr_in);
-
-		/* Wait a connection, and obtain a new socket file despriptor for single connection */
-		if ((nsockfd = accept(sockfd, (struct sockaddr *)&addr_remote, &sin_size)) == -1) 
-		{
-		    fprintf(stderr, "ERROR: Obtaining new Socket Despcritor. (errno = %d)\n", errno);
-			exit(1);
-		}
-		else 
-			printf("[Server] Server has got connected from %s.\n", inet_ntoa(addr_remote.sin_addr));
-
-		/*Receive File from Client */
-		char* fr_name = "/cs/home/himanshu/Desktop/final.txt";
-		FILE *fr = fopen(fr_name, "a");
-		if(fr == NULL)
-			printf("File %s Cannot be opened file on server.\n", fr_name);
-		else
-		{
-			bzero(revbuf, LENGTH); 
-			int fr_block_sz = 0;
-			while((fr_block_sz = recv(nsockfd, revbuf, LENGTH, 0)) > 0) 
-			{
-			    int write_sz = fwrite(revbuf, sizeof(char), fr_block_sz, fr);
-				if(write_sz < fr_block_sz)
-			    {
-			        error("File write failed on server.\n");
-			    }
-				bzero(revbuf, LENGTH);
-				if (fr_block_sz == 0 || fr_block_sz != 512) 
-				{
-					break;
-				}
-			}
-			if(fr_block_sz < 0)
-		    {
-		        if (errno == EAGAIN)
-	        	{
-	                printf("recv() timed out.\n");
-	            }
-	            else
-	            {
-	                fprintf(stderr, "recv() failed due to errno = %d\n", errno);
-					exit(1);
-	            }
-        	}
-			printf("Ok received from client!\n");
-			fclose(fr); 
-		}
-
-		/* Call the Script */
-		system("cd ; chmod +x script.sh ; ./script.sh");
-
-		/* Send File to Client */
-		//if(!fork())
-		//{
-		    char* fs_name = "/cs/home/himanshu/Desktop/himanshu.txt";
-		    char sdbuf[LENGTH]; // Send buffer
-		    printf("[Server] Sending %s to the Client...", fs_name);
-		    FILE *fs = fopen(fs_name, "r");
-		    if(fs == NULL)
-		    {
-		        fprintf(stderr, "ERROR: File %s not found on server. (errno = %d)\n", fs_name, errno);
-				exit(1);
-		    }
-
-		    bzero(sdbuf, LENGTH); 
-		    int fs_block_sz; 
-		    while((fs_block_sz = fread(sdbuf, sizeof(char), LENGTH, fs))>0)
-		    {
-		        if(send(nsockfd, sdbuf, fs_block_sz, 0) < 0)
-		        {
-		            fprintf(stderr, "ERROR: Failed to send file %s. (errno = %d)\n", fs_name, errno);
-		            exit(1);
-		        }
-		        bzero(sdbuf, LENGTH);
-		    }
-		    printf("Ok sent to client!\n");
-		    success = 1;
-		    close(nsockfd);
-		    printf("[Server] Connection with Client closed. Server will wait now...\n");
-		    while(waitpid(-1, NULL, WNOHANG) > 0);
-		//}
-	}
+/* Create a stream socket */
+if ((sd I s0cket(AF_1NET, SOCK_STREAM, 0)) I: -1) {
+fprintf(stderr, "Can't create a socket\n");
+exit(l);
+}
+/* Bind an address to the socket */
+bZer0((Char *)&server, siZeof(struct sockaddr_in));
+sezver.sin_family = AF_INET;
+server.sin_p0rt = ht0nS(p0rt);
+server.sin_addr.s_addr I hton1(INADDR_ANY);
+if (bind(sd, (struct sockaddr *)&server,
+sizeof(server)) I: -1) {
+fprintf(stderr, "Can't bind name to socket\n");
+exit(1);
+}
+/* queue up to 5 connect requests */
+listen(sd, 5);
+while (1) {
+c1ient_len = sizeof(client);
+if ((new_sd = accept(sd, (struct sockaddr *)
+&c1ient, &client_len)) == -1) {
+fprintf(stderr, "Can't accept client\n");
+exit(1);
+}
+bytes_to_read = BUFLEN;
+while ((n = read(new_sd, bp, bytes_to_read)) > O) {
+hp += n;
+bytes_to_read —= n;
+l
+write(new_sd, buf, BUFLEN);
+close(new_sd);
+}
+close(sd);
+return(O);
 }
